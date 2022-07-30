e229731e6510dbc4904f24c00cbc5d8134fb090d
Joaquin Gonzalez <GonzalezJoaquinEzequiel00@gmail.com>
2022-07-30 15:47:57 -0300

    Primer elemento con Javascript

---
+++ ciclo.js
@@ -0,0 +1,18 @@
+
+let parcial;
+let parcial2;
+let promedio;
+let suma;
+
+
+ parcial= parseInt(prompt("ingrese calificacion del primer parcial"));
+ parcial2= parseInt(prompt("ingrese calificacion del segundo parcial"));
+
+
+function sumar( parcial, parcial2 ); {
+ suma = parcial1 + parcial2
+ promedio = suma / 2
+ alert("el promedio de las notas es " + promedio);
+
+}
+ 
