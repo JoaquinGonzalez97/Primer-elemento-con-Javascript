2fdb09d64efbca51159c905807f386f771c7df84
Joaquin Gonzalez <GonzalezJoaquinEzequiel00@gmail.com>
2022-08-06 20:46:57 -0300

    agregando arrays

--- ciclo.js
+++ ciclo.js
@@ -3,7 +3,7 @@ let parcial;
 let parcial2;
 let promedio;
 let suma;
-
+const mensaje = ["aprobado", "desaprobado"];
 
  parcial= parseInt(prompt("ingrese calificacion del primer parcial"));
  parcial2= parseInt(prompt("ingrese calificacion del segundo parcial"));
@@ -14,5 +14,16 @@ function sumar( parcial, parcial2 ); {
  promedio = suma / 2
  alert("el promedio de las notas es " + promedio);
 
+ const mensaje = ["aprobado", "desaprobado"];
+
+for (promedio > 6) {
+    alert (mensaje[0]);
+    
 }
+
+if (promedio <= 6){
+    alert(mensaje[1]);
+}
+}
+
  
